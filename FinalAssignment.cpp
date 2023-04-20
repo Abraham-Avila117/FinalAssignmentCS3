e58888a6bd2b6631d41af42c23e17913368c9ea4
Abraham <aaavila@mavs.coloradomesa.edu>
2023-04-20 09:06:30 -0600

    new project

---
+++ FinalAssignment.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cassert>
+#include <iomanip>
+#include <fstream>
+#include <chrono>
+#include <cstdlib>
+#include <ctype>
+#include <cstring>
+
+using namespace std;
+
+int main(int argc, char** argv){
+    /*
+    *will clarify with Sherine
+    FinalAssignment tasks:
+    1. Hash table comprising of Open hasing (I-VI) and Linear probing (VII-XII)
+    2. Word ranking the top 80 words by occurences (most and least). Capitalization does not matter
+        and Double hyphen "--" must be removed. All outputs are streamed to a file from command line
+        argument. 
+    3. In IX, user must be prompt to do a word search, and use the Rabin-Karp pattern matching
+        and Horner's rule for the rolling hash. Display the word count and positions of the word 
+        from the user.
+    4. Develope the main function that is menu driven (0) to terminate and (99) to output everything. 
+        This will take in a command line argument of the input file and output file.
+    5. Demo presenter when assignment is turned in and is responsible for turning in the assignment.
+    
+    For each task, these must be reported:
+        1. Occupency Ratio. Report runtime in nanosec*
+        2. Open hashing - chain length optimality. 
+            Report 3 experiments and runtime in nanosec*
+        3. What hash function was used? Report 3 different 
+            experiements and runtime in nansec
+        4. Collision Resolutions (must use method from lectures)
+        5. Did you use a .h file to achive the tasks?
+        6. Make sure to label your section when outputing to the file
+        7. Everything will be output lower case except for Section title 
+    
+    */
+    return 0;
+}
