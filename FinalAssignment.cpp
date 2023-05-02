8335f60f32e79bc869827549c7d1d4b74f9669f1
Abraham <aaavila@mavs.coloradomesa.edu>
2023-05-01 21:56:06 -0600

    update on printIdx, and adding tasks.txt

--- FinalAssignment.cpp
+++ FinalAssignment.cpp
@@ -15,11 +15,12 @@ void readFile(ifstream&, Hash_chain<char*>&);
 bool checkTitle(char*);
 
 int sentenceCount = 0;
-const char* adventure[] = {"I.", "VII.", "XII.", "***"};
+const char* adventure[] = {"VII.", "IX", "XII.", "***"};
 
 struct Occur{
     char* word;
-    int rank = 0 ;
+    int rank = 0;
+    Occur* next;
 };
 
 int main(int argc, char** argv){
@@ -32,53 +33,8 @@ int main(int argc, char** argv){
 
     readFile(input, hash_chain);
     hash_chain.print();
+    // hash_chain.printIdx(541);
 
-    /*
-    FinalAssignment tasks:
-    
-    Update: readfile is now much shorter and less complecated. This task was
-    done my Abraham Avila. sentence counting is done
-
-    object for main will be hash
-    ~1. (Abraham) Hash table comprising of Open hasing with chaining (I-VI) 
-        1a. Count the number of occurences of the words/strings in the section 
-
-        1b. Answer (b) and (c) in assignment spec  
-
-    /2. (Ari) Linear probing (VII-XII)
-        2a. Count the number of occurences of the words/strings in the section
-
-        2b. Answer question (a), (c), and (d) in assignment spec
-
-    /3. (Connor) In IX, user must be prompt to do a word search, and use the Rabin-Karp pattern
-        matching and Horner's rule for the rolling hash. Display the word count and positions 
-        of the word from the user. This covers (f) in the assignment spec
-
-    4. (Ari)Word ranking the top 80 words by occurences (most and least). Capitalization does not matter
-        and Double hyphen "--" must be removed. All outputs are streamed to a file from command 
-        line argument. This covers (g) and (h) assignment spec
-
-    5. (Abraham) Develope the main driver program function that is menu driven (0) to terminate and (99) 
-        to output everything. This will take in a command line argument of the input file and 
-        output file.
-    
-    *6. (everyone: use global int variable)(solved by Abraham Avila using readFile) Develope 
-        sentence counting function. Be sure to clearly define what is a sentence and consult
-        team if definition works. This covers (i) in the 
-        assignment spec
-
-    7. (Connor) Demo presenter when assignment is turned in and is responsible for turning in the 
-        assignment. This task will be responsible for testing edge cases or errors and 
-        addressing them (debugger). 
-    
-    For each task, these must be reported:
-        1. Did you use a .h file to achive the tasks? (e) in spec
-        2. Make sure to label your section when outputing to the file
-        3. Everything will be output lower case except for Section title 
-        4. report runtime in nansec (exception: task 6). (j) in spec
-    
-    Note: will consult with sherine about (k) in spec
-    */
     return 0;
 }
 
@@ -88,7 +44,7 @@ void readFile(ifstream& infile, Hash_chain<char*>& h){
     char* fname = new char[fsize];
     char* sname = new char[ssize];
     char *tmp = nullptr, *pch;
-    char d[] = " \n\'\",;:-";
+    char d[] = " \n\'\",;:";
 
     while(!infile.eof()){
         infile >> fname[0];
@@ -121,7 +77,7 @@ void readFile(ifstream& infile, Hash_chain<char*>& h){
                     }
                     tmp = str.getList();
                     if(strcmp(tmp, "\0")!=0){
-                        h.insert(tmp, r%h.getSize());
+                        h.insertCharArray(tmp, r%h.getSize());
                     }
                     str.clear();
                     r = 0;      
@@ -134,91 +90,6 @@ void readFile(ifstream& infile, Hash_chain<char*>& h){
     delete [] fname;
     delete [] sname;
 }
-
-// void readFile(ifstream& infile, Hash_chain<char*>& h){
-
-//     Vector<char> vector;
-//     Vector<char*> sentence;
-//     int size = 81, r = 0, max = 0;
-//     char* fname = new char[size], *str;
-//     assert(fname!=nullptr);
-
-//         try{
-//         while(!infile.eof())
-//         {
-//             infile >> fname[0];
-//             infile.getline(&fname[1], size, '\n');  
-//             if(strncmp(fname, "ADVENTURE I. A SCANDAL IN BOHEMIA", 33)==0){
-
-//                 // go until start of sentence
-//                 memset(fname, '\0', size);
-//                 do{     
-//                     infile >> fname[0];
-//                     infile.getline(&fname[1], size);
-//                 }while(fname[0]!='T');
-
-//                 // insert to hash table and count sentences
-//                 while(!infile.eof()){
-//                     for(int i = 0; i < strlen(fname); i++){
-//                         if(i+1 == strlen(fname)){
-//                             if(vector.isEmpty())
-//                                 continue;
-//                             str = vector.getList();
-//                             h.insert(str, r%h.getSize());
-//                             r = 0;
-//                             vector.clear();
-//                             str = nullptr;
-//                             break;
-//                         }  
-//                         else if(fname[i] == '-' && fname[i+1] == '-'){
-//                             fname[i] = ' ';
-//                             fname[i+1] = ' ';
-//                         }
-//                         else if(fname[i] >= 65 && fname[i] <= 90 || fname[i] >= 97 && fname[i] <= 122 || fname[i] == '-'){
-//                             fname[i] = tolower(fname[i]);
-//                             vector.push(fname[i]);
-//                             r += fname[i];
-//                         }
-//                         else if(fname[i] == ' ' || fname[i] == ',' || fname[i] == ';' || fname[i] == ':'){
-//                             if(vector.isEmpty())
-//                                 continue;
-//                             str = vector.getList();
-//                             sentence.push(str);
-//                             h.insert(str, r%h.getSize());
-//                             if(max < r%h.getSize()) max = r%h.getSize();
-//                             vector.clear();
-//                             r = 0;
-//                             str = nullptr;
-//                         }
-//                         else if(fname[i] == '.' || fname[i] == '?' || fname[i] == '!'){
-//                             str = vector.getList();
-//                             h.insert(str, r%h.getSize());
-//                             sentence.push(str);
-//                             if(!checkTitle(str) && vector.getsize() > 1){
-//                                 sentenceCount++;
-//                                 sentence.clear();
-//                             }
-//                             vector.clear();
-//                             r = 0;
-//                             str = nullptr;
-//                         }
-//                     }
-//                     memset(fname, '\0', size);
-//                     infile >> fname[0];
-//                     infile.getline(&fname[1], size); 
-
-//                     if(strncmp(fname, "VII. THE ADVENTURE OF THE BLUE CARBUNCLE", 41)==0)
-//                         return; 
-//                 }
-//             }
-//         }
-//         sentence.clear();
-//         vector.clear();
-//         delete [] fname; 
-//     }catch(bad_alloc& ex){
-//         cout << ex.what() << endl;
-//     }
-// }
 
 bool checkTitle(char* check){
     const char* title[] = {"mr","mrs","dr", "prof", "ms",
--- Hash_chain.h
+++ Hash_chain.h
@@ -29,11 +29,16 @@ public:
     Hash_chain(T, int);
     int getSize()const;
     void insert(T, int);
+    void insertCharArray(char*, int);
     void print()const;
+    void printIdx(int)const;
     bool isEmpty()const;
     bool exceedSize(int)const;
     bool isIn(T, int)const;
-    void sort();
+    bool isInChar(char*,int)const; //this is specific to char*
+    int* rankListMost();
+    int* rankListLeast();
+    void sortRank();
 
     ~Hash_chain();
 
@@ -73,6 +78,50 @@ Hash_chain<T>::Hash_chain(T initalize, int size){
 template <class T>
 int Hash_chain<T>::getSize()const{
     return size;
+}
+
+template <class T>
+void Hash_chain<T>::insertCharArray(char* newitem, int idx){
+    try{
+        if(exceedSize(idx))
+            throw idx;           
+        else if(isInChar(newitem, idx)){
+            return;
+        }
+        else if(array[idx].item != buffer){
+            Node<T>* curr;
+            if(array[idx].next == nullptr){
+                curr = new Node<T>;
+                assert(curr!=nullptr);
+                curr->item = newitem;
+                curr->rank++;
+                curr->next = nullptr;
+                curr->tail = &array[idx];
+                array[idx].next = curr;
+                array[idx].tail = curr;
+                curr = nullptr;
+                return;
+            }
+
+            curr = new Node<T>;
+            assert(curr!=nullptr);
+            curr->item = newitem;
+            curr->rank++;
+            curr->next = nullptr;
+            array[idx].tail->next = curr;
+            curr->tail = array[idx].tail;
+            array[idx].tail = curr;
+            curr = nullptr;
+            return;   
+        }
+        else if(array[idx].item == buffer){
+            array[idx].item = newitem;
+            array[idx].rank++;
+        }
+    }catch(out_of_range& ex){
+        cerr << ex.what() << endl;
+        return;
+    }
 }
 
 template <class T>
@@ -139,6 +188,21 @@ void Hash_chain<T>::print()const{
 }
 
 template <class T>
+void Hash_chain<T>::printIdx(int idx)const{
+    Node<T>* tmp = array[idx].next;
+
+    cout << array[idx].item <<
+    " rank: " << array[idx].rank << " -> ";
+
+    while(tmp != nullptr){
+        cout << tmp->item << 
+        " rank: " << tmp->rank << ", ";
+        tmp = tmp->next;
+    }
+    cout << "|" << endl;
+}
+
+template <class T>
 bool Hash_chain<T>::isEmpty()const{
     return size == 0;
 }
@@ -152,6 +216,34 @@ bool Hash_chain<T>::exceedSize(int idx)const{
 template <class T>
 bool Hash_chain<T>::isIn(T search, int place)const{
     Node<T>* tmp;
+    if(array[place].item == buffer)
+        return false;
+    else if(array[place].item == search){
+        array[place].rank++;
+        tmp = nullptr;
+        return true;
+    }
+    else{
+        tmp = array[place].next;
+        while(tmp != nullptr){
+            if(tmp->item == search){
+                tmp->rank++;
+                if(tmp->rank > array[place].rank){
+
+                }
+                tmp = nullptr;
+                return true;
+            }
+            tmp = tmp->next;
+        }
+    }
+    tmp = nullptr;
+    return false;
+}
+
+template <class T>
+bool Hash_chain<T>::isInChar(char* search, int place)const{
+    Node<T>* tmp;
     if(array[place].item == nullptr)
         return false;
     else if(strcmp(array[place].item,search)==0){
@@ -164,6 +256,14 @@ bool Hash_chain<T>::isIn(T search, int place)const{
         while(tmp != nullptr){
             if(strcmp(tmp->item,search)==0){
                 tmp->rank++;
+                if(tmp->rank > array[place].rank){
+                    char* sw = array[place].item;
+                    int iw = array[place].rank;
+                    array[place].item = tmp->item;
+                    array[place].rank = tmp->rank;
+                    tmp->item = sw;
+                    tmp->rank = iw;
+                }
                 tmp = nullptr;
                 return true;
             }
@@ -172,6 +272,36 @@ bool Hash_chain<T>::isIn(T search, int place)const{
     }
     tmp = nullptr;
     return false;
+}
+
+template <class T>
+int* Hash_chain<T>::rankListMost(){
+    int* most = new int[80];
+    sortRank();
+    return most;
+}
+
+template <class T>
+int* Hash_chain<T>::rankListLeast(){
+    int* most = new int[80];
+    sortRank();
+    return most;
+}
+
+template <class T>
+void Hash_chain<T>::sortRank(){
+    Node<T>* tmp;
+    for(int i = 0; i < size; i++){
+        tmp = array[i].next;
+        if(tmp == nullptr) continue;
+
+        
+
+
+
+
+
+    }
 }
 
 template <class T>
