01bf1c8438ae12900e9354e02f419d38aab5875f
Sharp-Glass <128325209+Sharp-Glass@users.noreply.github.com>
2023-05-10 01:19:22 -0600

    Update Occur.h
    fix errors that abraham found.

--- Occur.h
+++ Occur.h
@@ -25,13 +25,13 @@ public:
     void clear();   // clear the list
     bool isEmpty()const; // check if list is empty
     bool isFull()const;  // check if list is full to increase size
-    // void print() const;
+    void print() const;
     void sort();// start the sort
     void sort(Occurence<T> *, int, int); //recersive call for the sort
     void merge(Occurence<T> *, int const, int const,int const); // merge sort fuction
     Occurence<T> * getList();       //get list so far
-    T * getWordList();
-    int *getRankList();
+    T * getWordList(); // list of only words
+    int *getRankList();// list of only ranks
     int getsize()const; //get the size of list so far
 
     ~Occur();
@@ -118,9 +118,7 @@ void Occur<T>::push(T elem){
 template <class T>
 int Occur<T>::search(T elem){
     for(int i = 0; i < maxSize; i++){
-       // cout<<"i=="<<i<<endl;
         if(vec[i].word==elem){
-            //cout<<"ret=="<<i<<endl;
             return i;
         }
     }
@@ -176,7 +174,7 @@ template <class T>
 void Occur<T>::sort(Occurence<T> *array, int begin, int end){
     if (begin >= end)
         return;
-    int mid = begin + (end - begin) / 2;
+    int mid = begin + (end - begin)/2;
     sort(array, begin, mid);
     sort(array, mid + 1, end);
     merge(array, begin, mid, end);
@@ -186,7 +184,7 @@ template <class T>
 void Occur<T>::sort(){
     if (0 >= size)
         return;
-    int mid = 0 + (size - 0) / 2;
+    int mid = (size)/2;
     sort(vec, 0, mid);
     sort(vec, mid + 1, size);
     merge(vec, 0, mid, size);
@@ -273,11 +271,22 @@ template <class T>
 int* Occur<T>::getRankList()
 {
     int * list = new int[size];
-    for(int i=0; i< size-1; i++)
+    for(int i=0; i< size; i++)
     {
         list[i]=vec[i].rank;
     }
     return list;
+}
+
+template <class T>
+void Occur<T>::print()const
+{
+    
+    for(int i=0; i< size; i++)
+    {
+        cout << vec[i].word << " | "<< vec[i].rank << endl;
+    }
+    
 }
 
 template <class T>
